61f41ba9a3e57b9c6e9b568438159649cb9c9300
Drago <jrsiquara@gmail.com>
2021-11-16 21:47:16 -0300

    Adicionado, funções de alteração e exclusão de dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     #path('', views.index),
     path('', RedirectView.as_view(url='/agenda/')), # Mas esta forma pareCe ser a melhor.
     path('login/', views.login_user),
